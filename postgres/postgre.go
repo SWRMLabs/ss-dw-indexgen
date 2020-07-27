21bc9c8b399ee81f45c6bfb9e9d0db626656b1a2
rajkumar081997 <rajkumar@stream.space>
2020-07-27 10:48:49 +0530

    added logs in getBCN

--- postgres/postgre.go
+++ postgres/postgre.go
@@ -95,6 +95,7 @@ func getBCN(timestamp int64, db *sql.DB) (int64, error) {
 	for rows.Next() {
 		err := rows.Scan(&bcn)
 		if err != nil {
+			log.Errorf("Scan of bcn is failed %s",err.Error())
 			return 0, err
 		}
 	}
